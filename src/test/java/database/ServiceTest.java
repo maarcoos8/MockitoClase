0a037e428394784c6660d6068c19b9db9c5c1fb3
maarcoos8 <maarcoos_8@uma.es>
2025-03-25 11:12:38 +0100

    Cambio el code por el del profe

--- src/test/java/database/ServiceTest.java
+++ src/test/java/database/ServiceTest.java
@@ -2,46 +2,63 @@ package database;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.booleanThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import javax.xml.crypto.Data;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ServiceTest {
+
+    @Test
+        public void query_WhenDatabaseAvailable_ReturnTrue() {
+        Database databaseMock = mock(Database.class);
+            when(databaseMock.isAvailable()).thenReturn(true);
+            Service t = new Service(databaseMock); 
+
+            boolean b = t.query("* from t");
+
+            assertTrue(b);
+            verify(databaseMock, times(1)).isAvailable();
+        }
+
+        @Test
+        @DisplayName("El metodo query devuelve true con una consulta")
+        public void query_SelectAll_ReturnTrue(){
+            //Arrange
+            Database database = mock(Database.class);
+            Service service = new Service(database);
+            when(database.isAvailable()).thenReturn(true);
+
+            //Act
+            boolean result = service.query("Select * from uma");
+
+            //Asssert
+            assertTrue(result);
+        }
+
+        @Test
+        public void toString_Users42_ReturnCorrectString()
+        {
+            //Arrange
+            int number = 42;
+            String expected ="Users in the database : " +number;
+            Database database = mock(Database.class);
+            Service service = new Service(database);
+            when(database.getNumberofUsers()).thenReturn(number);
+
+            //Act
+            String result= service.toString();
+
+            //Assert
+            assertEquals(expected, result);
+
+        }
+
     
-
-    @Test
-    @DisplayName("El metodo query devuelve el true con una consulta")
-    public void query_SelectAll_ReturnTrue(){
-
-        // Arrange
-        Database database = mock(Database.class);
-        Service service = new Service( database );
-        when( database.isAvailable()).thenReturn(true);
-
-        // Act
-        boolean ok = service.query("SELECT * FROM users");
-
-        // Assert
-        assertTrue( ok , "El metodo query deberia devolver true, en este caos hubo un error en el método");
-    }
-
-    @Test
-    @DisplayName("El metodo toString")
-    public void toString_User42_ReturnCorrectString(){
-        
-        // Arrange
-        int number = 42;
-        String resultExpected = "Users in the database : " + number;
-        Database database = mock(Database.class);
-        Service service = new Service( database );
-        when( database.getNumberofUsers()).thenReturn(number);
-
-        // Act
-        String result = service.toString();
-
-        // Assert
-        assertEquals(resultExpected, result);
-    }
 }
